ca61bb55deebc8cb6178d3d01db586757f1725e6
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-07-04 22:21:01 -0700

    Connected to firebase and added update methods

--- src/App.js
+++ src/App.js
@@ -1,21 +1,72 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import fire from './fire'
+
+// Material UI Components
+import { MuiThemeProvider } from 'material-ui/styles'
+import { TextField, Button } from 'material-ui'
+
+const database = fire.database().ref()
+const toolsRef = database.child('tools')
 
 class App extends Component {
+  state = {
+    newItem: '',
+  }
+
+  componentWillMount() {
+    toolsRef.on('value', snap => {
+      this.setState({ tools: snap.val() })
+    })
+  }
+
+  incrementButton = () => {
+    const likes = { likes: this.state.likes + 1 }
+  }
+  addNewTool = () => {
+    const { newItem } = this.state
+
+    // Don't submit if text field is empty
+    if (!newItem) {
+      document.getElementById('addnew').focus()
+      return false
+    }
+
+    // New Tool Data
+    const toolData = {
+      title: newItem,
+      likes: 0,
+    }
+
+    // New Tool Key
+    const newToolKey = toolsRef.push().key
+
+    const updates = {}
+    updates[`/tools/${newItem}${newToolKey}`] = toolData
+
+    database.update(updates)
+  }
+  renderList = () => {
+    return <div />
+  }
   render() {
+    const { likes } = this.state
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div>
+          <TextField
+            id="addnew"
+            label="Add New"
+            value={this.state.newItem}
+            onChange={event => this.setState({ newItem: event.target.value })}
+          />
+
+          <Button onClick={this.addNewTool}>
+            Add New
+          </Button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      </MuiThemeProvider>
+    )
   }
 }
 
-export default App;
+export default App
