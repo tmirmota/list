bbc3f9d6c3b1843cd2113c30bcf474201d446c17
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-07-10 21:33:47 -0700

    Remove Grid and added Table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -3,6 +3,13 @@ import PropTypes from 'prop-types'
 
 // Material UI
 import { withStyles, createStyleSheet } from 'material-ui/styles'
+import Typography from 'material-ui/Typography'
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from 'material-ui/Table'
 import Grid from 'material-ui/Grid'
 import Paper from 'material-ui/Paper'
 import Button from 'material-ui/Button'
@@ -12,6 +19,9 @@ import Divider from 'material-ui/Divider'
 import KeyboardArrowUp from 'material-ui-icons/KeyboardArrowUp'
 
 const styleSheet = createStyleSheet('DataTable', theme => ({
+  root: {
+    flexGrow: 1,
+  },
   button: {
     margin: theme.spacing.unit,
   },
@@ -29,38 +39,64 @@ class DataTable extends Component {
 
     // Render table once tools have returned
     return (
-      <div className="p-5">
-        <h3>Name</h3>
-        {tools.map(tool => {
-          const userLikes = likedTools[tool.key]
-          const userUpvoted = userLikes ? userLikes.like : false
-          return (
-            <section key={tool.key}>
-              <Grid container gutter={24} className="py-3">
-                <Grid item xs={6}>
-                  <a href={tool.url} className="lead" target="_blank">
-                    {tool.title}
-                  </a>
-                </Grid>
-                <Grid item xs={6} className="text-right">
-                  <Button
-                    fab
-                    color={userUpvoted ? 'accent' : 'default'}
-                    onClick={() => handleLike(tool)}
-                    className={classes.button}
-                  >
-                    <KeyboardArrowUp />
-                    {tool.likes}
-                  </Button>
-                </Grid>
-              </Grid>
-              <Divider />
-            </section>
-          )
-        })}
+      <div>
+        <Typography type="display1" color="inherit">
+          Top Tools for Digital Marketers
+        </Typography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Rank</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Votes</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {tools.map((tool, index) => {
+              // Tools the user has touched
+              const userLikes = likedTools[tool.key]
+              // Has the user upvoted this particular tool
+              const userUpvoted = userLikes ? userLikes.like : false
+              // Rank of current tool
+              const toolRank = index + 1
+
+              return (
+                <TableRow>
+                  <TableCell>
+                    {toolRank}
+                  </TableCell>
+                  <TableCell>
+                    <a href={tool.url} className="lead" target="_blank">
+                      {tool.title}
+                    </a>
+                  </TableCell>
+                  <TableCell>
+                    <Typography type="caption">This is a caption</Typography>
+                  </TableCell>
+                  <TableCell>
+                    <Button
+                      fab
+                      color={userUpvoted ? 'accent' : 'default'}
+                      onClick={() => handleLike(tool)}
+                      className={classes.button}
+                    >
+                      <KeyboardArrowUp />
+                      {tool.likes}
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
       </div>
     )
   }
+}
+
+DataTable.propTypes = {
+  classes: PropTypes.object.isRequired,
 }
 
 export default withStyles(styleSheet)(DataTable)
