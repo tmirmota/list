1b525f7d49d6071ee7027cd3a71efd372bf8bebd
Thomas Mirmo <thomas.mirmotahari@gmail.com>
2017-07-18 23:58:09 -0700

    Custom color for Facebook login button

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 // Material UI
+import { withStyles, createStyleSheet } from 'material-ui/styles'
 import Dialog, {
   DialogTitle,
   DialogContent,
@@ -8,6 +10,15 @@ import Dialog, {
 } from 'material-ui/Dialog'
 import Button from 'material-ui/Button'
 import Divider from 'material-ui/Divider'
+
+const styleSheet = createStyleSheet('SignIn', {
+  button: {
+    background: '#3B5998',
+    '&:hover': {
+      background: 'rgba(59, 89, 152, 0.8)',
+    },
+  },
+})
 
 class SignIn extends Component {
   handleFacebook = () => {
@@ -21,12 +32,17 @@ class SignIn extends Component {
   }
 
   render() {
-    const { open, onRequestClose } = this.props
+    const { open, onRequestClose, classes } = this.props
     return (
       <Dialog open={open} onRequestClose={onRequestClose}>
         <DialogTitle>Sign In to join the community</DialogTitle>
         <DialogContent className="text-center">
-          <Button raised color="primary" onClick={this.handleFacebook}>
+          <Button
+            raised
+            color="contrast"
+            className={classes.button}
+            onClick={this.handleFacebook}
+          >
             <i className="fa fa-facebook pr-2" />
             Login with Facebook
           </Button>
@@ -36,4 +52,8 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn
+SignIn.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styleSheet)(SignIn)
